631a1a3e4cb73a50f663c498e4ef12d74b5d0da1
p45308 <p45308@irma.ac.in>
2025-09-29 19:57:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def preprocess_input(data):
 
 # Make prediction when button is clicked
 if st.button("Predict Credit Risk"):
-    if loaded_model is not None and loaded_scaler is not None:
+    if loaded_model is not None:
         input_data = {
             'person_age': person_age,
             'person_income': person_income,
